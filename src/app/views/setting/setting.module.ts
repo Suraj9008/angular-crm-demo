74337ed964f09f184c735217c8bbe304143e78e3
suri@9008 <surajnarulesp108@gmail.com>
2021-06-18 17:17:38 +0530

    add field dinamicallyn

--- src/app/views/setting/setting.module.ts
+++ src/app/views/setting/setting.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http'
     ReactiveFormsModule,
     HttpClientModule
   ],
-  exports:[TexonomyService],
+  exports:[],
   providers:[TexonomyService]
 })
 export class SettingModule { }
